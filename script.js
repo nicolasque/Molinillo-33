110d8de3c12b885e06337532cbb3fa4e7d6610b8
nicolasque <37843574+nicolasque@users.noreply.github.com>
2023-04-28 03:11:42 +0200

    He añadido los tags para alergias

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const menuItems = {
     nuestros_tacos: [
       { name: 'Taco de suadero', price: '2,20€' },
       { name: 'taco de cochinita', price: '2,20€' },
-      { name: 'taco de carnitas', price: '2,20€' , tags: ['vegetariano']},
+      { name: 'taco de carnitas', price: '2,20€' },
 
     ],
     bebidas: [
@@ -39,16 +39,20 @@ const menuItems = {
       { name: 'Agua', price: '$1.49' },
     ],
     postre: [
-      { name: 'tarta de chocolate', price: '3€' },
+      { name: 'tarta de chocolate', price: '3€' , tags: ['celiaco']},
       { name: 'tarta de queso con fresa', price: '3€' },
       { name: 'tarta de manzana con dulce de leche', price: '3€' },
 
     ],
   };
   
+
+  // Seleccionamos los elementos del DOM
   const menuCategories = document.querySelector('.menu-categories');
   const menuContainer = document.querySelector('.menu-items');
   
+
+  // Funcion que crea los botones de las categorias
   function createCategoryButton(category) {
     const categoryBtn = document.createElement('li');
     categoryBtn.textContent = category.replace(/_/g, ' ');
@@ -61,6 +65,8 @@ const menuItems = {
     menuCategories.appendChild(categoryBtn);
   }
   
+
+  // Funcion que muestra los items de la categoria seleccionada
   function showCategoryItems(category) {
     menuContainer.innerHTML = '';
   
@@ -76,6 +82,9 @@ const menuItems = {
   
       menuItem.appendChild(menuItemName);
       menuItem.appendChild(menuItemPrice);
+
+      addIconsIfTagsExist(item, menuItem); // Llamar a la función para agregar íconos si el ítem tiene etiquetas
+
   
       menuContainer.appendChild(menuItem);
     });
@@ -83,6 +92,8 @@ const menuItems = {
     updateSelectedCategory(category);
   }
   
+  
+  // Funcion que actualiza el boton de la categoria seleccionada
   
 function updateSelectedCategory(selectedCategory) {
   const categoryButtons = document.querySelectorAll('.menu-categories li');
@@ -102,10 +113,55 @@ function updateSelectedCategory(selectedCategory) {
   showCategoryItems('tapas');
   
 
+
+  // Scroll image
   const scrollImageContainer = document.querySelector('.scroll-image-container');
 const scrollImage = document.querySelector('.scroll-image');
 
+
+// Actualizamos la opacidad de la imagen en funcion del scroll
 window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   scrollImage.style.opacity = 1 - scrollTop / scrollImageContainer.offsetHeight;
 });
+
+
+// funcion añadir etiqueta vegana, celiaca .......
+
+function getSvgPathForTag(tag) {
+  switch (tag) {
+    case 'vegetariano':
+      return '<svg> ... </svg>'; // Reemplaza con el código SVG o la ruta al archivo SVG para el ícono vegetariano
+    case 'celiaco':
+      return '<img src="img/alergenos/Gluten.svg" alt="con gluten"/>'; // Reemplaza con el código SVG o la ruta al archivo SVG para el ícono sin gluten
+    case 'sin_lactosa':
+      return '<svg> ... </svg>'; // Reemplaza con el código SVG o la ruta al archivo SVG para el ícono sin lactosa
+    // Agrega más casos según las etiquetas que necesites
+    default:
+      return '';
+  }
+}
+
+
+
+function addIconsIfTagsExist(item, menuItem) {
+  if (item.tags) {
+    // Crear contenedor de etiquetas
+    const menuItemTags = document.createElement('div');
+    menuItemTags.classList.add('menu-item-tags');
+
+    // Crear y agregar los íconos SVG para cada etiqueta
+    item.tags.forEach((tag) => {
+      const tagIcon = document.createElement('div');
+      tagIcon.classList.add('tag-icon');
+      
+      const svgPath = getSvgPathForTag(tag);
+      tagIcon.innerHTML = svgPath;
+
+      menuItemTags.appendChild(tagIcon);
+    });
+
+    // Añadir contenedor de etiquetas
+    menuItem.appendChild(menuItemTags);
+  }
+}
